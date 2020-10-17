efb6530d726878e5d60b02449320b770bf1394c7
Kamil Kuliś <k.kuliss@gmail.com>
2020-10-17 20:26:19 +0200

    I will do it again

---
+++ main.js
@@ -0,0 +1,16 @@
+const cardsColor = ['red', 'red', 'green', 'green', 'blue', 'blue', 'brown', 'brown', 'yellow', 'yellow', 'gray', 'gray', 'cadetblue', 'cadetblue', 'violet', 'violet', 'lightgreen', 'lightgreen'];
+
+let cards = document.querySelectorAll('div');
+
+cards = [...cards];
+
+
+const init = () => {
+
+cards.forEach ( card => {
+
+
+})
+
+}
+
